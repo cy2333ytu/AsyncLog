4b0a3b10829ff64d92778cb038ed56f5f41e3d2d
ccy <ccy.com>
2024-01-26 10:49:57 +0800

    achieve processTask && convert2Str()

--- AsyncLog.h
+++ AsyncLog.h
@@ -29,7 +29,6 @@ public:
     LogTask(const LogTask&& src):
         _logLevel{src._logLevel}, _logDatas{std::move(src._logDatas)}{}
 
-private:
     LogLevel _logLevel;
     std::queue<std::any> _logDatas;
     
@@ -49,8 +48,70 @@ public:
         _emptyCond.notify_one();
     }
 private:
+    AsyncLog& operator = (const AsyncLog&) = delete;
+    AsyncLog(const AsyncLog&) = delete;
+    AsyncLog(): _stop{false}{
+        workthread = std::thread([this](){
+            for(;;){
+                std::unique_lock<std::mutex> lock(_mtx);
+                while(_queue.empty() && !_stop){
+                    _emptyCond.wait(lock);
+                }
+                if(_stop){
+                    return;
+                }
+                auto logtask = _queue.front();
+                _queue.pop();
+                lock.unlock();
+                processTask(logtask);
+            }
+        });
+    }
+    void processTask(std::shared_ptr<LogTask> task){
+        std::cout << "log level is: " << task->_logLevel << std::endl;
+        if(task->_logDatas.empty()) return;
+        auto head = task->_logDatas.front();
+        task->_logDatas.pop();
+        
+        std::string formatStr = "";
+        bool success = convert2Str(head, formatStr);
+        if(!success) return;
+        
+        while(!task->_logDatas.empty()){
+            auto data = task->_logDatas.front();
+            formatStr = formatString(formatStr, data);
+            task->_logDatas.pop();
+        }
+        std::cout<<"log string is: " << formatStr <<std::endl;
+
+    }
+    bool convert2Str(const std::any &data, std::string str){
+        std::ostringstream ss;
+        if(data.type() == typeid(int)){
+            ss << std::any_cast<int>(data);
+        }else if(data.type() == typeid(float)){
+            ss << std::any_cast<float>(data);
+        }else if(data.type() == typeid(double)){
+            ss << std::any_cast<double>(data);
+        }else if(data.type() == typeid(std::string)){
+            ss << std::any_cast<std::string>(data);
+        }else if(data.type() == typeid(char*)){
+            ss << std::any_cast<char*>(data);
+        }else if(data.type() == typeid(char const *)){
+            ss << std::any_cast<char const *>(data);
+        }else{
+            return false;
+        }
+        str = ss.str();
+        return true;
+    }
+    template<typename ...Args>
+    std::string formatString(const std::string& format, Args... args){
+
+    }
     bool _stop;
     std::condition_variable _emptyCond;
+    std::queue<std::shared_ptr<LogTask>> _queue;
     std::mutex _mtx;
     std::thread workthread;
 };
